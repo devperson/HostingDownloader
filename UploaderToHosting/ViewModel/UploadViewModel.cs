ab29288ccfadff9787ffc81dc40cdbe3ea9892e0
khasan <xusanrahimov@gmail.com>
2014-04-20 19:54:41 +0500

    Mysds

---
+++ UploaderToHosting/ViewModel/UploadViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UploaderToHosting.ViewModel
+{
+    public class UploadViewModel : BaseViewModel
+    { 
+        public int UploadingCount = 4;
+        private int CurrentPartIndex = 0;
+        private int AllPartCount = -1;
+        /// <summary>
+        /// Start multiple uploading simultaneously
+        /// </summary>
+        public void StartUpload()
+        {
+        }
+        /// <summary>
+        /// Uploads part
+        /// </summary>
+        private void UploadPart()
+        {
+        }
+
+        private void OnCompletedUpload()
+        {
+        }
+
+        /// <summary>
+        /// Gets next part index
+        /// </summary>
+        /// <returns>next part index</returns>
+        private int NextPartIndex()
+        {
+            //lock should be used here
+            return -1;
+        }
+    }
+}
